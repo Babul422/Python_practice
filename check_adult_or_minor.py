9a0b1d8d75ba9a858866524474783a425427938f
Babul422 <babulkr852117@gmail.com>
2025-07-13 16:12:46 +0530

    Create check_adult_or_minor.py

---
+++ check_adult_or_minor.py
@@ -0,0 +1,12 @@
+# Ask the user to enter their age
+age = int(input("Enter your age: "))
+
+# Check if the age is greater than 18
+if age > 18:
+    # If true, print 'Adult'
+    print("Adult")
+else:
+    # If age is 18 or less, print 'Minor'
+    print("Minor")
+
+           
