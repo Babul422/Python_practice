eda1a3ce6f62d12c9ba9940afa3a138c81c9d7f3
Babul422 <babulkr852117@gmail.com>
2025-07-15 12:10:40 +0530

    Create functions_max_of_three.py

---
+++ functions_max_of_three.py
@@ -0,0 +1,8 @@
+# Q5: Write a function that finds the maximum of three numbers
+
+def max_of_three(a, b, c):
+    return max(a, b, c)
+
+# Example usage
+result = max_of_three(10, 50, 30)
+print("Maximum:", result)  # Output: Maximum: 50
