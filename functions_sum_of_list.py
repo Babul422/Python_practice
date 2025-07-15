9b10b3fb54c5b947e596ddd14f4398a1f78a9c85
Babul422 <babulkr852117@gmail.com>
2025-07-15 12:07:59 +0530

    Create functions_sum_of_list.py

---
+++ functions_sum_of_list.py
@@ -0,0 +1,8 @@
+# Q4: Write a function that takes a list and returns the sum of all numbers
+
+def sum_list(numbers):
+    return sum(numbers)
+
+# Example usage
+result = sum_list([1, 2, 3, 4, 5])
+print("Sum of list:", result)  # Output: Sum of list: 15
